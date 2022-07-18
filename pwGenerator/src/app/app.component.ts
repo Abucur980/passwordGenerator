f070cb1a68702a18c6d289e08ca064676d8b3f2d
Andrei Bucur <abucur980@gmail.com>
2022-07-18 14:41:08 +0300

    add main functionality

--- pwGenerator/src/app/app.component.ts
+++ pwGenerator/src/app/app.component.ts
@@ -6,9 +6,54 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  lengthInput = 0;
+  isLetterChecked = false;
+  isNumberChecked = false;
+  isSpecialCharChecked = false;
   password = '';
-  getPass() {
-    return this.password = "a password";
+
+  onChangeLength(event : Event) {
+    const target = event.target as HTMLInputElement;
+    const theLength = parseInt(target.value);
+    if(!isNaN(theLength)) {
+      this.lengthInput = theLength;
+    }
   }
+
+  changeLetterCheckbox() {
+    this.isLetterChecked = !this.isLetterChecked;
+  }
+
+  changeNumberCheckbox() {
+    this.isNumberChecked = !this.isNumberChecked;
+  }
+
+  changeSpecialCHarCheckbox() {
+    this.isSpecialCharChecked = !this.isSpecialCharChecked;
+  }
+
+  getPass() {
+    // console.log(this.isLetterChecked, this.isNumberChecked, this.isSpecialCharChecked, this.lengthInput);
+    const nums = '0123456789';
+    const letters = 'abcdefghijklmnopqrstuvwyz';
+    const symbols = '!@#$%^&*()';
+    let validChars = '';
+    if (this.isLetterChecked) {
+      validChars += letters;
+    }
+    if (this.isNumberChecked) {
+      validChars += nums;
+    }
+    if (this.isSpecialCharChecked) {
+      validChars += symbols;
+    }
+    let generatedPass = '';
+    for (let i = 0; i < this.lengthInput; ++i) {
+      generatedPass += validChars[Math.floor(Math.random()*validChars.length)];
+    }
+    this.password = generatedPass;
+    return this.password;
+  }
+
 }
 
